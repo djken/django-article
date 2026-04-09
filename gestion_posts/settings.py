960edbfe94a52f47f68f68bd76b952e92e5c5caf
Jean-Kénel Dessources <jeankeneld@gmail.com>
2026-04-09 08:28:48 -0400

    hosting

--- gestion_posts/settings.py
+++ gestion_posts/settings.py
@@ -12,9 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-f62@6(a)=w%z(@a!12kh#bexnp*4cl$5jowy4fw&ojq+v#6y)g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['aken.pythonanywhere.com', 'localhost', '127.0.0.1'] 
 
 
 # Application definition
